a207c58322b4d830f45df974485b3fbbd0de3e2e
sanjave <ssingh025@citymail.cuny.edu>
2023-09-28 21:57:51 -0400

    Day 24: Exercise: Add lines

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
-    pdf.ln(255)
+    pdf.ln(258)
     pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -26,10 +28,13 @@ for index, row in df.iterrows():
         pdf.add_page()
 
         # Set the footer
-        pdf.ln(267)
+        pdf.ln(270)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
 
